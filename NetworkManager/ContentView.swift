3c2ec1d689d826eb15d0e45d3c857f5134b39f57
Devendra Kumar <devendra.kumar@nagarro.com>
2023-01-12 10:04:41 +0530

    Initial Commit

---
+++ NetworkManager/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NetworkManager
+//
+//  Created by Devendra Agnihotri on 12/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetworkManager/NetworkManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManagerApp.swift
+//  NetworkManager
+//
+//  Created by Devendra Agnihotri on 12/01/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
